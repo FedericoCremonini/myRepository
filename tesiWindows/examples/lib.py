55d7fa8a6213cc1e3c1bbe78ce83629276f4cb9f
FedericoCremonini <55797211+FedericoCremonini@users.noreply.github.com>
2020-03-30 16:50:17 +0200

    Update lib.py
    I add epipole research

--- tesiWindows/examples/lib.py
+++ tesiWindows/examples/lib.py
@@ -474,5 +474,127 @@ def flow_filter(flow, bound):
 
     return mask
 
+def findEpipole2(lines, n, p, x, y):
 
+    """
+    :param lines: equations of epipolar lines
+    :param n: number of iterations
+    :param p: number of threshold cases
+    :param x: width of the images
+    :param y: height of the images
+    :return coordX: coordinate x of epipole
+    :return coordY: coordinate y of epipole
+    """
 
+    best = false
+    bestX = -1
+    bestY = -1
+
+    res = [ [ 0 for i in range(y) ] for j in range(x) ]
+    #res = np.zeros((x,y))
+
+    for i in range(n):
+
+        r = lines[int(random.randrange(0, lines1.size/3))]; a = r[0] ; b = r[1] ; c = r[2] # a * x + b * y + c = 0
+        
+        for j in range(x):
+
+            coordY = int(-(a * j  + c)/b)
+
+            if(coordY >= 0 and coordY < y):
+                res[j][coordY] = res[j][coordY]+1
+
+                if(res[j][coordY] == 5):
+
+                    bestX = j
+                    bestY = coordY
+                    best = true
+
+            if(best):
+                break
+        if(best):
+            break
+
+    return bestX, bestY
+
+def drawlines(img1,img2,lines,pts1,pts2):
+    ''' img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines '''
+    r, c, ch = img1.shape
+    # Da utilizzare se utilizzato cv2.imread(img, 0)
+    #img1 = cv2.cvtColor(img1,cv2.COLOR_GRAY2BGR)
+    #img2 = cv2.cvtColor(img2,cv2.COLOR_GRAY2BGR)
+    for r,pt1,pt2 in zip(lines,pts1,pts2):
+        color = tuple(np.random.randint(0,255,3).tolist())
+        x0,y0 = map(int, [0, -r[2]/r[1] ])
+        x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
+        img1 = cv2.line(img1, (x0,y0), (x1,y1), color,1)
+        img1 = cv2.circle(img1,tuple(pt1),5,color,-1)
+        img2 = cv2.circle(img2,tuple(pt2),5,color,-1)
+    return img1,img2
+
+def findEpipole(new, old, n, p, x, y):
+
+    """
+    :param new: current image
+    :param new: previous image
+    :param n: number of iterations
+    :param p: number of threshold cases
+    :param x: width of the images
+    :param y: height of the images
+    :return coordX: coordinate x of epipole
+    :return coordY: coordinate y of epipole
+    """
+
+    #sift = cv2.xfeatures2d.SIFT_create()
+    orb = cv2.ORB_create(nfeatures=1500)
+
+    #per trovare epipole 
+  
+    keypointsOld, descriptorsOld = orb.detectAndCompute(old, None)
+    descriptorsOld = np.float32(descriptorsOld)
+    kpold = cv2.drawKeypoints(old, keypointsOld, None)
+
+    keypointsNew, descriptorsNew = orb.detectAndCompute(new, None)
+    descriptorsNew = np.float32(descriptorsNew)
+    kpnew = cv2.drawKeypoints(new, keypointsNew, None)
+  
+    # FLANN parameters ######################################################################
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(descriptorsNew,descriptorsOld,k=2)
+
+    good = []
+    pts1 = []
+    pts2 = []
+    # ratio test as per Lowe's paper
+    for i,(m,n) in enumerate(matches):
+        if m.distance < 0.85*n.distance:
+            good.append(m)
+            pts2.append(keypointsOld[m.trainIdx].pt)
+            pts1.append(keypointsNew[m.queryIdx].pt)
+    # We find fundmental matrix
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+    F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
+    # We select only inlier points
+    pts1 = pts1[mask.ravel()==1]
+    pts2 = pts2[mask.ravel()==1]
+    # Find epilines corresponding to points in old image (second image) and
+    # drawing its lines on new image
+    lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
+    lines1 = lines1.reshape(-1,3)
+    #img5, img6 = drawlines(new,old,lines1,pts1,pts2)
+    # Find epilines corresponding to points in new image (first image) and
+    # drawing its lines on old image
+    lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
+    lines2 = lines2.reshape(-1,3)
+    #img3, img4 = drawlines(old,new,lines2,pts2,pts1)
+ 
+    #find epipole
+
+    coordX, coordY = findEpipole2(lines1, n, p, x, y)
+
+    return coordX, coordY
